e07dab11d6794eb83e853e52adef6561f0275756
angelshipmanagement1-maker <your-email@example.com>
2025-08-08 11:28:59 +0530

    Revert to simple configuration that worked for Kalpesh and Bhushan - use relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Chandan_yadav_portfolio/' : '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 })
